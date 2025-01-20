18d7e361f840a38d96d305d93318e06a17d87bb4
hoyeonj981 <hoyeonj981@gmail.com>
2025-01-21 04:43:02 +0900

    style: google guide에 맞게 수정

--- src/main/java/me/hoyeon/shortlink/ShortlinkApplication.java
+++ src/main/java/me/hoyeon/shortlink/ShortlinkApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShortlinkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShortlinkApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ShortlinkApplication.class, args);
+  }
 }
