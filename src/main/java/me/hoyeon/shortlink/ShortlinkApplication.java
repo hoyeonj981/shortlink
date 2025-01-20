f07c519e105ab1752d8070549cd48c46b4914b5e
hoyeonj981 <hoyeonj981@gmail.com>
2025-01-20 10:19:01 +0900

    Initial commit

---
+++ src/main/java/me/hoyeon/shortlink/ShortlinkApplication.java
@@ -0,0 +1,13 @@
+package me.hoyeon.shortlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortlinkApplication.class, args);
+	}
+
+}
