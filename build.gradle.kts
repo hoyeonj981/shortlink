44a8745f226203a3749aa8ec89a584f89490b65f
hoyeonj981 <hoyeonj981@gmail.com>
2025-01-21 22:13:50 +0900

    chore: 환경 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.asciidoctor.jvm.convert") version "3.3.2"
 	id("jacoco")
-	id("org.sonarqube") version "4.4.1.3373"
+	id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "me.hoyeon"
