44a0483701f24078e8d21ff010233ac0dfda71ab
hoyeonj981 <hoyeonj981@gmail.com>
2025-01-21 20:48:33 +0900

    feat: 더미 클래스와 1 더하기 기능 추가

---
+++ src/main/java/me/hoyeon/shortlink/DummyClass.java
@@ -0,0 +1,14 @@
+package me.hoyeon.shortlink;
+
+public class DummyClass {
+
+  private int value;
+
+  public DummyClass(final int value) {
+    this.value = value;
+  }
+
+  public int addOne() {
+    return this.value++;
+  }
+}
