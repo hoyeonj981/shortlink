912b452b2e9d087a8162c94b3918201f279843d8
hoyeonj981 <hoyeonj981@gmail.com>
2025-01-26 03:13:07 +0900

    feat: 임시 URL 단축 알고리즘 구현

---
+++ src/main/java/me/hoyeon/shortlink/domain/SimpleAliasGenerator.java
@@ -0,0 +1,38 @@
+package me.hoyeon.shortlink.domain;
+
+import java.util.Objects;
+
+public class SimpleAliasGenerator implements AliasGenerator {
+
+  private static final int FIXED_LENGTH = 6;
+  private static final String BASE62
+      = "0123456789"
+      + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      + "abcdefghijklmnopqrstuvwxyz";
+
+  @Override
+  public String shorten(String base) {
+    var hashed = Objects.hashCode(base);
+    return convertHashToAlias(hashed);
+  }
+
+  private String convertHashToAlias(int hashed) {
+    return encodeBase62(hashed);
+  }
+
+  private String encodeBase62(int value) {
+    var unsignedValue = Integer.toUnsignedLong(value);
+    var result = new StringBuilder();
+
+    do {
+      result.insert(0, BASE62.charAt((int) (unsignedValue % 62)));
+      unsignedValue /= 62;
+    } while (unsignedValue > 0);
+
+    while (result.length() < FIXED_LENGTH) {
+      result.insert(0, 0);
+    }
+
+    return result.toString();
+  }
+}
