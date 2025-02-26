1711daafab909c00047ed66544a0d32dab2d0b32
hoyeonj981 <hoyeonj981@gmail.com>
2025-02-27 03:32:49 +0900

    Revert "feat: 동등 조건 추가"
    This reverts commit 9f93195586525046ee52054b1cda11d18ff1b3d5.

--- src/main/java/me/hoyeon/shortlink/domain/VerificationToken.java
+++ src/main/java/me/hoyeon/shortlink/domain/VerificationToken.java
@@ -3,7 +3,7 @@ package me.hoyeon.shortlink.domain;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Objects;
+import java.time.temporal.ChronoUnit;
 
 public class VerificationToken {
 
@@ -43,20 +43,5 @@ public class VerificationToken {
     var now = Instant.now(clock);
     var between = Duration.between(createdAt, now);
     return between.compareTo(validDuration) > 0;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    VerificationToken that = (VerificationToken) o;
-    return Objects.equals(token, that.token) && Objects.equals(validDuration,
-        that.validDuration);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(token, validDuration);
   }
 }
