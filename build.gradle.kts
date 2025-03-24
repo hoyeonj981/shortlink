1243dbb09811c0075e7dfa2fcc379d65d5bf6090
hoyeonj981 <hoyeonj981@gmail.com>
2025-03-24 17:42:18 +0900

    feat: java-jwt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,7 +69,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-mail");
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("com.auth0:java-jwt:4.5.0")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
