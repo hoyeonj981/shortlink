d73f2d18f9db57e17daae433c250b469cce4344c
hoyeonj981 <hoyeonj981@gmail.com>
2025-04-15 14:58:20 +0900

    refactor: 기존 테스트 케이스를 nested 방식으로 변경

--- src/test/java/me/hoyeon/shortlink/integration/ui/UrlControllerStandAloneSetupTest.java
+++ src/test/java/me/hoyeon/shortlink/integration/ui/UrlControllerStandAloneSetupTest.java
@@ -12,11 +12,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.hoyeon.shortlink.application.ShortenedResult;
+import me.hoyeon.shortlink.application.UrlNotFoundException;
 import me.hoyeon.shortlink.application.UrlShortenerService;
 import me.hoyeon.shortlink.ui.CreateShortUrlRequest;
 import me.hoyeon.shortlink.ui.UrlController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -38,35 +40,40 @@ public class UrlControllerStandAloneSetupTest {
     objectMapper = new ObjectMapper();
   }
 
-  @DisplayName("POST 성공 /api/v1/urls - 유효한 요청을 보내면 단축된 URL을 생성 후 응답을 반환한다")
-  @Test
-  void returnShortUrlResponseIfRequestIsValid() throws Exception {
-    var originalUrl = "https://example.com";
-    var alias = "abcdef";
-    var request = new CreateShortUrlRequest(originalUrl);
-    when(urlShortenerService.shortenUrl(any())).thenReturn(new ShortenedResult(originalUrl, alias));
+  @DisplayName("요청 성공")
+  @Nested
+  class SuccessCases {
 
-    mockMvc.perform(post("/api/v1/urls")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(objectMapper.writeValueAsString(request)))
-        .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.originalUrl", is(originalUrl)))
-        .andExpect(jsonPath("$.alias", is(alias)));
+    @DisplayName("POST /api/v1/urls - 유효한 요청을 보내면 단축된 URL을 생성 후 응답을 반환한다")
+    @Test
+    void returnShortUrlResponseIfRequestIsValid() throws Exception {
+      var originalUrl = "https://example.com";
+      var alias = "abcdef";
+      var request = new CreateShortUrlRequest(originalUrl);
+      when(urlShortenerService.shortenUrl(any())).thenReturn(new ShortenedResult(originalUrl, alias));
 
-    verify(urlShortenerService).shortenUrl(any());
-  }
+      mockMvc.perform(post("/api/v1/urls")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(request)))
+          .andExpect(status().isCreated())
+          .andExpect(jsonPath("$.originalUrl", is(originalUrl)))
+          .andExpect(jsonPath("$.alias", is(alias)));
 
-  @DisplayName("GET 성공 /api/v1/urls/{alias} - 유효한 요청을 보내면 단축 URL의 원본을 반환한다")
-  @Test
-  void getOriginalUrlIfRequestIsValid() throws Exception {
-    var alias = "abcdef";
-    var originalUrl = "https://example.com";
-    when(urlShortenerService.getOriginalUrl(alias)).thenReturn(originalUrl);
+      verify(urlShortenerService).shortenUrl(any());
+    }
 
-    mockMvc.perform(get("/api/v1/urls/{alias}", alias))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.originalUrl", is(originalUrl)));
+    @DisplayName("GET /api/v1/urls/{alias} - 유효한 요청을 보내면 단축 URL의 원본을 반환한다")
+    @Test
+    void getOriginalUrlIfRequestIsValid() throws Exception {
+      var alias = "abcdef";
+      var originalUrl = "https://example.com";
+      when(urlShortenerService.getOriginalUrl(alias)).thenReturn(originalUrl);
 
-    verify(urlShortenerService).getOriginalUrl(alias);
+      mockMvc.perform(get("/api/v1/urls/{alias}", alias))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.originalUrl", is(originalUrl)));
+
+      verify(urlShortenerService).getOriginalUrl(alias);
+    }
   }
 }
