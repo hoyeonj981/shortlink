361dab0c56d888f28ada62b6918c79fa6385851d
hoyeonj981 <hoyeonj981@gmail.com>
2025-04-22 09:35:14 +0900

    feat: 글로벌 예외 핸들러 구현 및 요청 실패 테스트 케이스 추가

---
+++ src/main/java/me/hoyeon/shortlink/config/exception/ErrorResponse.java
@@ -0,0 +1,6 @@
+package me.hoyeon.shortlink.config.exception;
+
+public record ErrorResponse(
+    String code,
+    String message
+) { }
--- src/test/java/me/hoyeon/shortlink/integration/ui/UrlControllerStandAloneSetupTest.java
+++ src/test/java/me/hoyeon/shortlink/integration/ui/UrlControllerStandAloneSetupTest.java
@@ -11,9 +11,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.hoyeon.shortlink.application.DuplicateUrlException;
+import me.hoyeon.shortlink.application.NotAccessibleUrlException;
 import me.hoyeon.shortlink.application.ShortenedResult;
 import me.hoyeon.shortlink.application.UrlNotFoundException;
 import me.hoyeon.shortlink.application.UrlShortenerService;
+import me.hoyeon.shortlink.config.exception.GlobalExceptionHandler;
 import me.hoyeon.shortlink.ui.CreateShortUrlRequest;
 import me.hoyeon.shortlink.ui.UrlController;
 import org.junit.jupiter.api.BeforeEach;
@@ -36,6 +39,7 @@ public class UrlControllerStandAloneSetupTest {
   void setUp() {
     urlShortenerService = mock(UrlShortenerService.class);
     mockMvc = MockMvcBuilders.standaloneSetup(new UrlController(urlShortenerService))
+        .setControllerAdvice(new GlobalExceptionHandler())
         .build();
     objectMapper = new ObjectMapper();
   }
@@ -73,6 +77,45 @@ public class UrlControllerStandAloneSetupTest {
           .andExpect(status().isOk())
           .andExpect(jsonPath("$.originalUrl", is(originalUrl)));
 
+      verify(urlShortenerService).getOriginalUrl(alias);
+    }
+  }
+
+  @DisplayName("요청 실패")
+  @Nested
+  class FailureCases {
+
+    @DisplayName("GET /api/v1/urls/{alias} - 존재하지 않는 alias는 404 에러를 반환한다")
+    @Test
+    void return404IfAliasDoesNotExist() throws Exception {
+      var alias = "abcdef";
+      when(urlShortenerService.getOriginalUrl(alias)).thenThrow(UrlNotFoundException.class);
+
+      mockMvc.perform(get("/api/v1/urls/{alias}", alias))
+          .andExpect(status().isNotFound());
+    }
+
+    @DisplayName("POST /api/v1/urls - 중복된 URL 요청은 404 Bad Request를 반환한다")
+    @Test
+    void return404IfUrlIsDuplicated() throws Exception {
+      var duplicatedUrl = "https://example.com";
+      var request = new CreateShortUrlRequest(duplicatedUrl);
+      when(urlShortenerService.shortenUrl(any())).thenThrow(DuplicateUrlException.class);
+
+      mockMvc.perform(post("/api/v1/urls")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(objectMapper.writeValueAsString(request)))
+          .andExpect(status().isBadRequest());
+    }
+
+    @DisplayName("GET /api/v1/urls/{alias} - 만료된 alias는 410 에러를 반환한다")
+    @Test
+    void return401IfAliasIsExpired() throws Exception {
+      var alias = "abcdef";
+      when(urlShortenerService.getOriginalUrl(alias)).thenThrow(NotAccessibleUrlException.class);
+
+      mockMvc.perform(get("/api/v1/urls/{alias}", alias))
+          .andExpect(status().isGone());
       verify(urlShortenerService).getOriginalUrl(alias);
     }
   }
