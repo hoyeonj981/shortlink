cd71b713783437f68550b12354011df1a15bd5d7
hoyeonj981 <hoyeonj981@gmail.com>
2025-05-03 02:54:11 +0900

    feat: 단일 사용자 조회 서비스 구현

---
+++ src/test/java/me/hoyeon/shortlink/application/MemberQueryServiceTest.java
@@ -0,0 +1,73 @@
+package me.hoyeon.shortlink.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import me.hoyeon.shortlink.domain.Email;
+import me.hoyeon.shortlink.domain.EmailValidator;
+import me.hoyeon.shortlink.domain.InvalidEmailException;
+import me.hoyeon.shortlink.domain.Member;
+import me.hoyeon.shortlink.domain.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MemberQueryServiceTest {
+
+  @Mock
+  private MemberRepository memberRepository;
+
+  @Mock
+  private EmailValidator emailValidator;
+
+  @InjectMocks
+  private MemberQueryService memberQueryService;
+
+  @DisplayName("이메일로 회원 조회 시 해당 회원이 있으면 성공적으로 회원을 반환한다")
+  @Test
+  void getMemberByEmailSuccessfully() {
+    var emailAddress = "test@example.com";
+    doNothing().when(emailValidator).validate(emailAddress);
+    var email = Email.from(emailAddress, emailValidator);
+    var member = mock(Member.class);
+    when(memberRepository.findByEmail(email)).thenReturn(Optional.of(member));
+
+    var result = memberQueryService.getMemberByEmail(emailAddress);
+
+    assertThat(result).isEqualTo(member);
+    verify(memberRepository).findByEmail(email);
+  }
+
+  @DisplayName("이메일로 회원 조회 시 해당 회원이 없으면 예외가 발생한다")
+  @Test
+  void throwExceptionWhenMemberNotFoundDuringGetMemberByEmail() {
+    var emailAddress = "test@example.com";
+    doNothing().when(emailValidator).validate(emailAddress);
+    var email = Email.from(emailAddress, emailValidator);
+    when(memberRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+    assertThatThrownBy(() -> memberQueryService.getMemberByEmail(emailAddress))
+        .isInstanceOf(MemberNotFoundException.class);
+    verify(memberRepository).findByEmail(email);
+  }
+
+  @DisplayName("이메일 형식이 올바르지 않으면 예외가 발생한다")
+  @Test
+  void throwExceptionWhenEmailFormatIsInvalid() {
+    var invalidEmail = "invalid-email";
+    when(Email.from(invalidEmail, emailValidator))
+        .thenThrow(new InvalidEmailException(invalidEmail));
+
+    assertThatThrownBy(() -> memberQueryService.getMemberByEmail(invalidEmail))
+        .isInstanceOf(InvalidEmailException.class);
+  }
+}
