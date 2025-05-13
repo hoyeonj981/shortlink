6f67f9176b9cebf970f225f6c37e528cf605e037
hoyeonj981 <hoyeonj981@gmail.com>
2025-05-13 12:14:34 +0900

    refactor: 직접 토큰 추출 방법에서 JwtTokenProvider를 통해서 claim 추출 방법으로 변경

--- src/main/java/me/hoyeon/shortlink/infrastructure/security/JwtAuthenticationProvider.java
+++ src/main/java/me/hoyeon/shortlink/infrastructure/security/JwtAuthenticationProvider.java
@@ -1,10 +1,16 @@
 package me.hoyeon.shortlink.infrastructure.security;
 
-import com.auth0.jwt.JWT;
+import static me.hoyeon.shortlink.infrastructure.JwtClaimKey.MEMBER_ID;
+import static me.hoyeon.shortlink.infrastructure.JwtClaimKey.ROLE;
+
+import java.util.Collection;
+import java.util.Collections;
 import me.hoyeon.shortlink.application.JwtTokenProvider;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public class JwtAuthenticationProvider implements AuthenticationProvider {
 
@@ -18,13 +24,17 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
   public Authentication authenticate(Authentication authentication) throws AuthenticationException {
     var token = (String) authentication.getCredentials();
     jwtTokenProvider.validate(token);
-    var memberId = JWT.decode(token).getClaim("memberId");
-    // role 구현필요
-    return new JwtAuthenticationToken(memberId, token, null);
+    var memberId = jwtTokenProvider.getClaim(token, MEMBER_ID.getClaimName());
+    var role = jwtTokenProvider.getClaim(token, ROLE.getClaimName());
+    return new JwtAuthenticationToken(memberId, token, createAuthority(role));
   }
 
   @Override
   public boolean supports(Class<?> authentication) {
     return JwtAuthenticationToken.class.isAssignableFrom(authentication);
+  }
+
+  private Collection<? extends GrantedAuthority> createAuthority(String role) {
+    return Collections.singleton(new SimpleGrantedAuthority(role));
   }
 }
