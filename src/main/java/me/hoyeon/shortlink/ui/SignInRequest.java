0014e7fe5824133c9b4ed1f034b459a0794a7179
hoyeonj981 <hoyeonj981@gmail.com>
2025-05-15 05:16:56 +0900

    feat: 요청에 대한 validation 조건 추가

--- src/main/java/me/hoyeon/shortlink/ui/SignInRequest.java
+++ src/main/java/me/hoyeon/shortlink/ui/SignInRequest.java
@@ -1,7 +1,14 @@
 package me.hoyeon.shortlink.ui;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record SignInRequest(
+    @Email
+    @NotBlank
     String email,
+
+    @NotBlank
     String rawPassword
 ) {
 
