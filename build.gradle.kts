aec988f0494a4df73783bc62bc467f30d90e4978
hoyeonj981 <hoyeonj981@gmail.com>
2025-05-19 13:52:08 +0900

    feat: MockWebServer 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
