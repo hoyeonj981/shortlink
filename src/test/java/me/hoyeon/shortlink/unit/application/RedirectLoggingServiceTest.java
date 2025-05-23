752f6571bd7a47285f66290845c6e59e11f24416
hoyeonj981 <hoyeonj981@gmail.com>
2025-05-23 17:33:26 +0900

    refactor: 로그 기록과 조회 클래스 분리

--- src/test/java/me/hoyeon/shortlink/unit/application/RedirectLoggingServiceTest.java
+++ src/test/java/me/hoyeon/shortlink/unit/application/RedirectLoggingServiceTest.java
@@ -1,11 +1,8 @@
 package me.hoyeon.shortlink.unit.application;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import me.hoyeon.shortlink.application.AccessLogReader;
 import me.hoyeon.shortlink.application.AccessLogWriter;
 import me.hoyeon.shortlink.application.RedirectInfo;
 import me.hoyeon.shortlink.application.RedirectLoggingService;
@@ -18,9 +15,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class RedirectLoggingServiceTest {
-
-  @Mock
-  private AccessLogReader accessLogReader;
 
   @Mock
   private AccessLogWriter accessLogWriter;
@@ -43,18 +37,5 @@ class RedirectLoggingServiceTest {
     redirectLoggingService.log(testInfo);
 
     verify(accessLogWriter, times(1)).write(testInfo);
-  }
-
-  @DisplayName("getTotalCount는 AccessLogReader의 getTotalCount 결과를 반환한다")
-  @Test
-  void getTotalCountShouldReturnReaderResult() {
-    var expectedCount = 5L;
-    var givenAlias = "abcdef";
-    when(accessLogReader.getTotalCount(givenAlias)).thenReturn(expectedCount);
-
-    var result = redirectLoggingService.getTotalCount(givenAlias);
-
-    assertThat(result).isEqualTo(expectedCount);
-    verify(accessLogReader, times(1)).getTotalCount(givenAlias);
   }
 }
