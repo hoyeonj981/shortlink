2e971a3e104d794b43c2e202cc5b187f96a72f0a
hoyeonj981 <hoyeonj981@gmail.com>
2025-05-31 12:09:26 +0900

    refactor: JPQL 삭제 및 통계 조회 인터페이스 분리

--- src/main/java/me/hoyeon/shortlink/infrastructure/AccessLogJpaRepository.java
+++ src/main/java/me/hoyeon/shortlink/infrastructure/AccessLogJpaRepository.java
@@ -1,43 +1,10 @@
 package me.hoyeon.shortlink.infrastructure;
 
-import java.time.LocalDate;
-import java.util.List;
-import me.hoyeon.shortlink.application.CountryStatisticDto;
-import me.hoyeon.shortlink.application.DailyStatisticDto;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccessLogJpaRepository extends JpaRepository<AccessLog, Long> {
+public interface AccessLogJpaRepository extends JpaRepository<AccessLog, Long>, AccessLogQueryRepository {
 
   Long countByAlias(String alias);
-
-  @Query("SELECT new me.hoyeon.shortlink.application.DailyStatisticDto("
-      + "DATE(a.redirectedAt), COUNT(a)) "
-      + "FROM AccessLog a "
-      + "WHERE a.alias = :alias AND a.redirectedAt BETWEEN :from AND :to "
-      + "GROUP BY DATE(a.redirectedAt) "
-      + "ORDER BY DATE(a.redirectedAt) ASC")
-  List<DailyStatisticDto> findDailyStatByAlias(
-      @Param("alias") String alias,
-      @Param("from") LocalDate from,
-      @Param("to") LocalDate to,
-      Pageable pageable
-  );
-
-  @Query("SELECT new me.hoyeon.shortlink.application.CountryStatisticDto("
-      + "a.country, COUNT(a)) "
-      + "FROM AccessLog a "
-      + "WHERE a.alias = :alias AND a.redirectedAt BETWEEN :from AND :to "
-      + "GROUP BY a.country "
-      + "ORDER BY COUNT(a) DESC")
-  List<CountryStatisticDto> findCountryStatByAlias(
-      @Param("alias") String alias,
-      @Param("from") LocalDate from,
-      @Param("to") LocalDate to,
-      Pageable pageable
-  );
 }
